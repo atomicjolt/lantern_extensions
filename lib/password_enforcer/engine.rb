9b5dae7994cb73faae6089ee3664db23a8021333
Ryker Blunck <ryker.blunck@atomicjolt.com>
2020-10-28 19:53:23 +0000

    add a custom path for canvas to search for messages

--- lib/password_enforcer/engine.rb
+++ lib/password_enforcer/engine.rb
@@ -1,14 +1,20 @@
 module PasswordEnforcer
   class Engine < ::Rails::Engine
+    isolate_namespace PasswordEnforcer
+
     config.to_prepare do
       Canvas::PasswordPolicy.define_singleton_method :default_policy do
-       {
-         # :max_repeats => 2,
-         # :max_sequence => 3,
-         # :disallow_common_passwords => true,
-         :min_length => 10
-       }
+        {
+          :max_repeats => 2,
+          :max_sequence => 3,
+          :disallow_common_passwords => true,
+          :min_length => 10
+        }
       end
+
+      # ApplicationController.prepend_view_path "gems/plugins/password_enforcer/app/views/password_enforcer"
+      # Mailer.prepend_view_path "gems/plugins/password_enforcer/app/messages"
+      Canvas::MessageHelper.add_message_path(File.join(Rails.root.to_s, 'gems', 'plugins', 'password_enforcer', 'messages'))
     end
   end
 end
