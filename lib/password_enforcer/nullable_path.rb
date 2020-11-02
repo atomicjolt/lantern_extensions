9e49ad844185963da1bc29f5ddd1987caac7ec02
Ryker Blunck <ryker.blunck@atomicjolt.com>
2020-11-02 16:23:16 +0000

    add the canvas plugin. it can be enabled in the UI, and both password enforcement and email templates check if it's enabled

--- lib/password_enforcer/nullable_path.rb
+++ lib/password_enforcer/nullable_path.rb
@@ -2,7 +2,7 @@ module PasswordEnforcer
   class NullablePath
     def to_str
       base_path = File.join(Rails.root.to_s, 'gems', 'plugins', 'password_enforcer', 'messages')
-      if File.exist?(File.join(Rails.root.to_s, 'enable'))
+      if Canvas::Plugin.find(:password_enforcer).enabled?
         base_path
       else
         # this is a path that is known to not exist
