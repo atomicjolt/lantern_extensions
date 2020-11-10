71b625bb170d01f6b49dcf6ff6aaf6bfe1d40e03
Ryker Blunck <ryker.blunck@atomicjolt.com>
2020-11-10 22:29:06 +0000

    only check that the plugin exists when initially loading. This avoids the weirdness with nullable path, and will make far fewer calls to check if the plugin is enabled. Also checks the table exists so that initial_setup can succeed

--- lib/lantern_extensions/engine.rb
+++ lib/lantern_extensions/engine.rb
@@ -12,22 +12,19 @@ module LanternExtensions
         settings_partial: 'lantern_extensions/plugin_settings'
       })
 
-      Canvas::PasswordPolicy.define_singleton_method :default_policy do
-        if Canvas::Plugin.find(:lantern_extensions).enabled?
+      if ActiveRecord::Base.connection.table_exists?('plugin_settings') && Canvas::Plugin.find(:lantern_extensions).enabled?
+        Canvas::PasswordPolicy.define_singleton_method :default_policy do
           {
             :max_repeats => 2,
             :max_sequence => 3,
             :disallow_common_passwords => true,
             :min_length => 10
           }
-        else
-          { :min_length => 8 }
         end
-      end
 
-      # ApplicationController.prepend_view_path "gems/plugins/lantern_extensions/app/views/lantern_extensions"
-      # Mailer.prepend_view_path "gems/plugins/lantern_extensions/app/messages"
-      Canvas::MessageHelper.add_message_path(LanternExtensions::NullablePath.new)
+        message_path = File.join(Rails.root.to_s, 'gems', 'plugins', 'lantern_extensions', 'messages')
+        Canvas::MessageHelper.add_message_path(message_path)
+      end
     end
   end
 end
